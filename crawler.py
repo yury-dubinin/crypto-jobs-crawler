7d5bee71b44076e321078f17c3d0ee7f5332747e
Yury Dubinin <yury.dubinin@sdx.com>
2023-04-03 22:52:21 +0200

    up 3-apr

--- crawler.py
+++ crawler.py
@@ -39,9 +39,11 @@ with open('web3.html', 'w') as f:
     f.write('<!DOCTYPE html>')
 
 # setup headless webdriver
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(options=chrome_options)
 
 
 def filter_jobs(job_title: str, filters):
