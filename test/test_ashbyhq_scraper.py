fc8a5f5c197a5efbedf55aebe4d09bf1dac76a97
yury-dubinin <yury.dubinin@gmail.com>
2023-04-07 12:28:22 +0200

    added syndica and ellipsislabs
    up 7-apr

--- test/test_ashbyhq_scraper.py
+++ test/test_ashbyhq_scraper.py
@@ -5,14 +5,14 @@ from src.scrape_ashbyhq import ScrapeAshbyhq
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
-company_list = [CompanyItem('syndica', 'https://jobs.ashbyhq.com/syndica', ScrapeAshbyhq, 'https://www.sygnum.com',
+companies = [CompanyItem('syndica', 'https://jobs.ashbyhq.com/syndica', ScrapeAshbyhq, 'https://www.sygnum.com',
                             'Crypto bank'),
-                CompanyItem('solana-foundation', 'https://jobs.ashbyhq.com/Solana%20Foundation', ScrapeAshbyhq, 'https://www.sygnum.com',
+             CompanyItem('solana-foundation', 'https://jobs.ashbyhq.com/Solana%20Foundation', ScrapeAshbyhq, 'https://www.sygnum.com',
                             'Crypto bank'),
-                CompanyItem('ellipsislabs', 'https://jobs.ashbyhq.com/ellipsislabs', ScrapeAshbyhq, 'https://ellipsislabs.xyz', 'Trading Protocol')
-                ]
+             CompanyItem('ellipsislabs', 'https://jobs.ashbyhq.com/ellipsislabs', ScrapeAshbyhq, 'https://ellipsislabs.xyz', 'Trading Protocol')
+             ]
 
-for company in company_list:
+for company in companies:
     print(company.jobs_url)
     data = company.scraper_type().getJobs(driver, company.jobs_url)
     for entry in data:
