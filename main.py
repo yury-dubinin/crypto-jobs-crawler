57bb14c27a40d3e3156360b36861d17c92d9ec0e
yakuramori <62520712+yury-dubinin@users.noreply.github.com>
2024-07-03 18:06:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import os
 vercel_url = "osmosis-frontend-qtpf9r606-osmo-labs.vercel.app"
 gh_out = f"environment_url={vercel_url}"
 os.system(f'echo "{gh_out}" >> $GITHUB_OUTPUT')
+os.system(f'echo "two=one" >> $GITHUB_OUTPUT')
 os.system(f'echo "{gh_out}" >> $GITHUB_ENV')
+os.system(f'echo "one=bar" >> $GITHUB_ENV')
 os.system(f'echo Vercel deployment: "{gh_out}" >> $GITHUB_STEP_SUMMARY')
